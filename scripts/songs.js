2a23b7b52c3390f73325c855357e72814ad05b00
kotaid <71237415+kotaid@users.noreply.github.com>
2021-01-02 10:37:22 +0100

    Update songs.js

--- scripts/songs.js
+++ scripts/songs.js
@@ -4,7 +4,7 @@ let songs = [
     cover:
       "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
     artist: "Hakim Omari",
-    audio: "./songs/quran.mp3",
+    audio: null,
     id: 0,
   },
   {
@@ -12,7 +12,7 @@ let songs = [
     cover:
       "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
     artist: "Hakim Omari1",
-    audio: "./songs/quran1.mp3",
+    audio: null,
     id: 1,
   },
   {
@@ -20,7 +20,7 @@ let songs = [
     cover:
       "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
     artist: "Hakim Omari2",
-    audio: "./songs/quran2.mp3",
+    audio: null,
     id: 2,
   },
   {
@@ -28,7 +28,7 @@ let songs = [
     cover:
       "https://chillhop.com/wp-content/uploads/2020/07/ef95e219a44869318b7806e9f0f794a1f9c451e4-1024x1024.jpg",
     artist: "Hakim Omari3",
-    audio: "./songs/quran3.mp3",
+    audio: null,
     id: 3,
   },
   {
@@ -36,7 +36,7 @@ let songs = [
     cover:
       "https://chillhop.com/wp-content/uploads/2020/07/ff35dede32321a8aa0953809812941bcf8a6bd35-1024x1024.jpg",
     artist: "Hakim Omari4",
-    audio: "./songs/quran4.mp3",
+    audio: null,
     id: 4,
   },
   {
@@ -44,7 +44,50 @@ let songs = [
     cover:
       "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d-1024x1024.jpg",
     artist: "Hakim Omari5",
-    audio: "./songs/quran5.mp3",
+    audio: null,
     id: 5,
   },
 ];
+storageRef
+  .child("default")
+  .child("quran.mp3")
+  .getDownloadURL()
+  .then((url) => {
+    songs[0].audio = url;
+  });
+
+storageRef
+  .child("default")
+  .child("quran1.mp3")
+  .getDownloadURL()
+  .then((url) => {
+    songs[1].audio = url;
+  });
+storageRef
+  .child("default")
+  .child("quran2.mp3")
+  .getDownloadURL()
+  .then((url) => {
+    songs[2].audio = url;
+  });
+storageRef
+  .child("default")
+  .child("quran3.mp3")
+  .getDownloadURL()
+  .then((url) => {
+    songs[3].audio = url;
+  });
+storageRef
+  .child("default")
+  .child("quran4.mp3")
+  .getDownloadURL()
+  .then((url) => {
+    songs[4].audio = url;
+  });
+storageRef
+  .child("default")
+  .child("quran5.mp3")
+  .getDownloadURL()
+  .then((url) => {
+    songs[5].audio = url;
+  });
